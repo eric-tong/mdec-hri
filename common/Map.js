f16dcf1003d90d8dcea3fe6f0efa82dd54590608
eric-tong <eric.yintong@gmail.com>
2018-12-30 23:25:09 +0800

    MDEC-2 Refactor

--- common/Map.js
+++ common/Map.js
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
 import {MapStyle} from './styles/MapStyle.js';
+import {DefaultViewport} from './constants/DefaultViewport.js';
 
 export default function Map() {
-  const [viewport, setViewport] = useState({
-    width: '100%',
-    height: '100vh',
-    latitude: 51.7520,
-    longitude: -1.2577,
-    zoom: 15,
-  });
+  const [viewport, setViewport] = useState(DefaultViewport);
 
   const onViewportChange = viewport => {
     if (viewport.zoom >= 15) setViewport(viewport);
---
+++ common/constants/DefaultViewport.js
@@ -0,0 +1,7 @@
+export const DefaultViewport = {
+	width: '100%',
+	height: '100vh',
+	latitude: 51.7520,
+	longitude: -1.2577,
+	zoom: 15,
+}
