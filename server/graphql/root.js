27b62c940a3bd07abffbc9626398eeae5c3f5603
eric-tong <eric.yintong@gmail.com>
2019-01-06 18:14:45 +0000

    MDEC-5 Add circular drone motion

--- server/graphql/root.js
+++ server/graphql/root.js
@@ -2,11 +2,17 @@ import { defaultViewport } from '../../common/defaultViewport';
 
 export const root = {
     drones: () => {
+        const radius = 0.003;
+        const time = new Date().getTime() / 1000;
+        const bearing = time % 360;
+        const offsetX = -radius * Math.cos(bearing / 180 * Math.PI);
+        const offsetY = radius * Math.sin(bearing / 180 * Math.PI);
+
         return {
             id: 1,
-            longitude: defaultViewport.longitude, 
-            latitude: defaultViewport.latitude,
-            bearing: 45
+            longitude: defaultViewport.longitude + offsetX, 
+            latitude: defaultViewport.latitude + offsetY,
+            bearing: bearing
         };
     },
 };
