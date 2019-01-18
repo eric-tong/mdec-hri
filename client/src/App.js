61d77a7698ee6efc05be167a680d6fdac2730d06
Eric Tong <eric.yintong@gmail.com>
2019-01-18 12:56:29 +0000

    MDEC-5 Add overview card

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Map from './components/Map.js'
 
 function App() {
-    return <Map />
+    return <>
+    <Map />
+    <div class="overview"></div>
+    </>
 }
 export default App
