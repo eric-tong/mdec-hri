283c8285a08114cd70ce2bf044eaef0c65a5e8b5
Eric Tong <eric.yintong@gmail.com>
2019-01-18 15:00:52 +0000

    MDEC-5 Add sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Map from './components/Map.js'
+import OverviewCard from './components/OverviewCard.js';
+import './styles/base-styles.scss'
 
 function App() {
     return <>
     <Map />
-    <div class="overview"></div>
+    <OverviewCard />
     </>
 }
 export default App
