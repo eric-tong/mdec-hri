3dcb78db8fab497a588fc8209a57a15d66d9743f
eric-tong <eric.yintong@gmail.com>
2019-01-21 11:23:25 +0000

    MDEC-5 Remove overview card

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,6 @@ import React from 'react'
 import Map from './components/Map.js'
 
 function App() {
-    return <>
-    <Map />
-    <div class="overview"></div>
-    </>
+    return <Map />
 }
 export default App
