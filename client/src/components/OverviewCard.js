6551b83b2e050375dede0c1771913aa14e0df837
Eric Tong <eric.yintong@gmail.com>
2019-01-28 18:09:08 +0000

    MDEC-5 Add card styles

--- client/src/components/OverviewCard.js
+++ client/src/components/OverviewCard.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 export default function OverviewCard() {
     return <div class="overview">
-        <div class="">Test</div>
+        <h1>Overview</h1>
+        <div class="section">
+            <h2>Header</h2>
+            <p>Content</p>
+        </div>
+        <div class="section">
+            <h2>Header</h2>
+            <p>Content</p>
+        </div>
+        <div class="section">
+            <h2>Header</h2>
+            <p>Content</p>
+        </div>
     </div>;
 }
