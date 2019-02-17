461199cd175e99a558ff0797d4a64789c2b9a847
eric-tong <eric.yintong@gmail.com>
2019-02-17 16:06:50 +0000

    MDEC-6 Move drones by bearing and fixed velocity

--- simulator/getDrones.js
+++ simulator/getDrones.js
@@ -1,28 +1,24 @@
 const baseCoords = require('./baseCoords');
 
-function getDrones() {
-    const N = 3;
-    const time = new Date().getTime() / 99;
-    const radius = 0.003;
-    const drones = [];
-    
-            for (let i = 0; i < N; i++) {
-        const bearing = time % 360 + i * 45;
-        const offsets = getOffsets(bearing, radius * (i + 1) / 2);
-        drones.push({
-            id: i,
-            longitude: baseCoords.longitude + offsets[0],
-            latitude: baseCoords.latitude + offsets[1],
-            bearing: bearing
-        });
+const drones = Array.from({ length: 3 }, (v, k) => {
+    return {
+        id: k,
+        longitude: baseCoords.longitude,
+        latitude: baseCoords.latitude,
+        bearing: 45 * k,
     }
-    return drones;   
+})
+
+function getDrones() {
+    drones.forEach(move);
+    return drones;
 }
 
-function getOffsets(bearing, radius) {
-    const offsetX = -radius * Math.cos(bearing / 180 * Math.PI);
-    const offsetY = radius * Math.sin(bearing / 180 * Math.PI);
-    return [offsetX, offsetY];
+const velocity = 0.0001;
+
+function move(drone) {
+    drone.longitude += Math.sin(drone.bearing / 180 * Math.PI) * velocity;
+    drone.latitude += Math.cos(drone.bearing / 180 * Math.PI) * velocity;
 }
 
 module.exports = getDrones;
