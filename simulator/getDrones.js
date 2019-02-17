ec1cd4b8857fc8a0d5895eaa00c1b4767d092ada
eric-tong <eric.yintong@gmail.com>
2019-02-17 16:32:40 +0000

    MDEC-5 Set bearing to target

--- simulator/getDrones.js
+++ simulator/getDrones.js
@@ -1,24 +1,34 @@
 const baseCoords = require('./baseCoords');
 
-const drones = Array.from({ length: 3 }, (v, k) => {
+const drones = Array.from({ length: 1 }, (v, k) => {
     return {
         id: k,
         longitude: baseCoords.longitude,
         latitude: baseCoords.latitude,
-        bearing: 45 * k,
+        targetLongitude: baseCoords.longitude + 0.01,
+        targetLatitude: baseCoords.latitude + 0.01,
+        bearing: 0,
     }
 })
 
 function getDrones() {
+    drones.forEach(rotateToTarget);
     drones.forEach(move);
     return drones;
 }
 
-const velocity = 0.0001;
+
+function rotateToTarget(drone) {
+    dx = drone.targetLatitude - drone.latitude;
+    dy = drone.targetLongitude - drone.longitude;
+    drone.bearing = Math.atan(dx / dy) / Math.PI * 180;
+}
+
+const velocity = 0.0003;
 
 function move(drone) {
-    drone.longitude += Math.sin(drone.bearing / 180 * Math.PI) * velocity;
     drone.latitude += Math.cos(drone.bearing / 180 * Math.PI) * velocity;
+    drone.longitude += Math.sin(drone.bearing / 180 * Math.PI) * velocity;
 }
 
 module.exports = getDrones;
