3390e546cd7e1ec4058c4a6ebec9981249bc0ec4
eric-tong <eric.yintong@gmail.com>
2019-03-29 14:31:41 +0000

    MDEC-10 Refactor

--- server/findBestConnectivity.js
+++ server/findBestConnectivity.js
@@ -1,19 +1,26 @@
 function findBestConnectivity(base, target) {
+    // Set connectivity relative to base
     base.distance = 0;
 
+    // Initialise set of visited nodes and visit queue
     const visited = new Set();
     const queue = [base];
 
     while (queue.length > 0) {
         const node = queue.shift();
+
+        // For each adjacent node, update the distance
         node.adjacentNodes.forEach(([adjacentNode, weight]) => {
             if (!visited.has(adjacentNode)) {
+
+                // Update distance if new distance is smaller
                 const newDistance = node.distance + weight;
                 if (newDistance < adjacentNode.distance) {
                     adjacentNode.distance = newDistance;
                     adjacentNode.bestAdjacentNode = node;
                 }
 
+                // Add to visit queue if not target
                 if (adjacentNode !== target) {
                     queue.push(adjacentNode);
                 }
@@ -22,24 +29,14 @@ function findBestConnectivity(base, target) {
         visited.add(node);
     }
 
-    return getPath(target);
-}
-
-function getPath(node) {
-    const result = [];
+    // Get the path from base to target
+    const path = [];
+    let node = target;
     while (node) {
-        result.unshift(node);
+        path.unshift(node);
         node = node.bestAdjacentNode;
     }
-    return result;
-}
-
-function print(nodes) {
-    if (Array.isArray(nodes)) {
-        console.log(nodes.map(node => node.id));
-    } else {
-        console.log(nodes.id);
-    }
+    return path;
 }
 
 module.exports = findBestConnectivity;
