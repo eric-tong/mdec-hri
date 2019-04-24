4bbffe6e136720eb5e25c5ceb1b425914df00333
eric-tong <eric.yintong@gmail.com>
2019-04-24 11:22:13 +0100

    MDEC-0 Add dummy data

--- client/src/components/CardSection.js
+++ client/src/components/CardSection.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function CardSection() {
+export default function CardSection({ header, content }) {
     return <div class="section">
-        <h2>Header</h2>
-        <p>Content</p>
+        <h2>{header}</h2>
+        <p>{content}</p>
     </div>
 }
